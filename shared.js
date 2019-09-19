acc9ea9919447c54d0ecfc8e731e03f1edc80104
Saju Paul <saju.christel@blu-pa.com>
2019-09-19 19:15:02 +0530

    Manipulate by adding/removing class instead of inline styles

--- shared.js
+++ shared.js
@@ -7,24 +7,32 @@ var mobileNav = document.querySelector(".mobile-nav");
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener("click", function() {
-    modal.style.display = "block";
-    backdrop.style.display = "block";
+    // modal.style.display = "block";
+    // backdrop.style.display = "block";
+    // modal.className = 'open'; THis will remove all clas and add 'open';
+    modal.classList.add("open");
+    backdrop.classList.add("open");
   });
 }
 
 modalNoActionButton.addEventListener("click", closeModal);
 
 backdrop.addEventListener("click", function() {
-  mobileNav.style.display = "none";
+  // mobileNav.style.display = "none";
+  mobileNav.classList.remove("open");
   closeModal();
 });
 
 function closeModal() {
-  modal.style.display = "none";
-  backdrop.style.display = "none";
+  // modal.style.display = "none";
+  // backdrop.style.display = "none";
+  modal.classList.remove("open");
+  backdrop.classList.remove("open");
 }
 
 toggleButton.addEventListener("click", function() {
-  mobileNav.style.display = "block";
-  backdrop.style.display = "block";
+  // mobileNav.style.display = "block";
+  // backdrop.style.display = "block";
+  mobileNav.classList.add("open");
+  backdrop.classList.add("open");
 });
