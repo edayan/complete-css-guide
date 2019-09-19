904049687b9e8d44942514dfd5a9718d0ddd8fc0
Saju Paul <saju.christel@blu-pa.com>
2019-09-19 19:02:51 +0530

    Open close hamburger icon

--- shared.js
+++ shared.js
@@ -1,8 +1,9 @@
 var backdrop = document.querySelector(".backdrop");
-// console.dir(backdrop);
 var modal = document.querySelector(".modal");
-// console.dir(backdrop);
 var selectPlanButtons = document.querySelectorAll(".plan button");
+var modalNoActionButton = document.querySelector(".modal__action--negative");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener("click", function() {
@@ -11,14 +12,19 @@ for (var i = 0; i < selectPlanButtons.length; i++) {
   });
 }
 
-var modalNoActionButton = document.querySelector(
-  ".modal__action--negative"
-);
 modalNoActionButton.addEventListener("click", closeModal);
 
-backdrop.addEventListener("click", closeModal);
+backdrop.addEventListener("click", function() {
+  mobileNav.style.display = "none";
+  closeModal();
+});
 
-function closeModal () {
-    modal.style.display = "none";
-    backdrop.style.display = "none";
+function closeModal() {
+  modal.style.display = "none";
+  backdrop.style.display = "none";
 }
+
+toggleButton.addEventListener("click", function() {
+  mobileNav.style.display = "block";
+  backdrop.style.display = "block";
+});
