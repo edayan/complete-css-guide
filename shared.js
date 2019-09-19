339246818529e2852bb1147f1005bac4d819106b
Saju Paul <saju.christel@blu-pa.com>
2019-09-19 18:36:01 +0530

    Hiding modal on no/outside click

--- shared.js
+++ shared.js
@@ -1,12 +1,24 @@
-var backdrop = document.querySelector('.backdrop');
+var backdrop = document.querySelector(".backdrop");
 // console.dir(backdrop);
-var modal = document.querySelector('.modal');
+var modal = document.querySelector(".modal");
 // console.dir(backdrop);
-var selectPlanButtons = document.querySelectorAll('.plan button');
+var selectPlanButtons = document.querySelectorAll(".plan button");
 
-for(var i = 0; i < selectPlanButtons.length; i++) {
-    selectPlanButtons[i].addEventListener('click', function () {
-        modal.style.display='block';
-        backdrop.style.display='block';
-    });
+for (var i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener("click", function() {
+    modal.style.display = "block";
+    backdrop.style.display = "block";
+  });
+}
+
+var modalNoActionButton = document.querySelector(
+  ".modal__action--negative"
+);
+modalNoActionButton.addEventListener("click", closeModal);
+
+backdrop.addEventListener("click", closeModal);
+
+function closeModal () {
+    modal.style.display = "none";
+    backdrop.style.display = "none";
 }
